8d8992aef248ab6c62c06fada0793a83e34c7209
Dipha <diphawgna@gmail.com>
2026-03-31 14:06:33 +0700

    feat: add section experiences

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,13 +1,21 @@
 import Header from "@/components/header";
+import Contact from "@/components/sections/contact";
+import Experiences from "@/components/sections/experiences";
 import TechStack from "@/components/sections/technologies";
 
 export default function MainContent() {
   return (
-     <div className="bg-zinc-50 dark:bg-black space-y-20">
+    <div className="bg-zinc-50 dark:bg-black space-y-20">
       <Header />
-      <TechStack />
-
-      
+      <section id="technologies" className="scroll-mt-24">
+        <TechStack />
+      </section>
+      <section id="experience" className="scroll-mt-24">
+        <Experiences />
+      </section>
+      <section id="contact" className="scroll-mt-24">
+        <Contact />
+      </section>
     </div>
   );
 }
